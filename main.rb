0b71f35e572a0565056843a6b033121fd9afb2cd
Hunter Braun <hunter.braun@gmail.com>
2015-05-09 01:08:35 -0500

    Output initial data to XML

--- main.rb
+++ main.rb
@@ -1,20 +1,33 @@
-require_relative './lib/nokogiri'
+require_relative './nokogiri-master/lib/nokogiri'
 require 'open-uri'
+load 'alfred_feedback.rb'
 
-lastName = ARGV.first
-doc = Nokogiri::HTML(open('http://animalcrossing.wikia.com/wiki/' + lastName))
+name = ARGV.first
+# exit 1 unless code_from_user.length == 4
+doc = Nokogiri::HTML(open('http://animalcrossing.wikia.com/wiki/' + name))
 
 table = doc.css('#WikiaArticle table')
-
 rows = table.css('tr')
-
 data = {}
 
 rows.each do |r|
   columns = r.css('td')
   if columns.length == 2
-    data[columns[0].content.strip] = columns[1].content.strip
+    data[columns[0].content.lstrip.chomp] = columns[1].content.lstrip.chomp
   end
 end
 
-print data
+feedback = Feedback.new
+feedback.add_item(
+  title:        name,
+  subtitle:     "#{data['Personality']}",
+  uid:          'animalcrossing',
+  arg:          'http://animalcrossing.wikia.com/wiki/' + name,
+  autocomplete: 'AC',
+  icon:         {
+    type:       'filetype',
+    name:       'icon.png'
+  }
+)
+
+puts feedback.to_xml
