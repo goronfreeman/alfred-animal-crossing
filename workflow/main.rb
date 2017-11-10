86af32d867bbab26211425fe029a12f05e8ae871
Hunter Braun <hunter.braun@gmail.com>
2017-11-09 19:40:04 -0600

    Get trait autocomplete working

--- workflow/main.rb
+++ workflow/main.rb
@@ -7,33 +7,35 @@ require 'alfred-3_workflow'
 $LOAD_PATH << '.'
 require 'villagers'
 
+require 'fileutils'
 require 'open-uri'
+require 'net/https'
 
 module AlfredAC
   class Search
     include AlfredAC::Villagers
 
-    attr_reader :query, :category, :workflow, :info
+    attr_reader :query, :trait, :workflow, :info
 
     def initialize
-      @query = ARGV[0]&.downcase
-      @category = ARGV[1]&.downcase
+      @query = ARGV[0].downcase
+      @trait = ARGV.drop(1).each(&:downcase).join(' ') if ARGV.length > 1
       @workflow = Alfred3::Workflow.new
       @info = AlfredAC::Villagers.info
     end
 
     def search
-      # TODO: Account for Agent S and Wart Jr.
+      # TODO: Account for villagers with a space (i.e. Agent S, Wart Jr)
       if villager_match(query)
         data = find_data(query)
 
-        if category
-          categories = find_categories(data, category)
+        if trait
+          traits = find_traits(data, trait)
 
-          return no_match_json if categories.empty?
-          find_categories(data, category).each { |k, v| category_json(k, v) }
+          return no_match_json if traits.empty?
+          find_traits(data, trait).each { |k, v| trait_json(k, v) }
         else
-          data.each { |k, v| category_json(k, v) }
+          data.each { |k, v| trait_json(k, v) }
         end
       else
         villagers = find_matches(query)
@@ -113,11 +115,11 @@ module AlfredAC
       info.keys.include?(name)
     end
 
-    def find_categories(data, category)
-      data.select { |k, _v| k =~ Regexp.new(category, Regexp::IGNORECASE) }
+    def find_traits(data, trait)
+      data.select { |k, _v| k =~ Regexp.new(trait, Regexp::IGNORECASE) }
     end
 
-    def icon_name(trait)
+    def icon_path(trait)
       "img/#{trait.split.join('_')}.png"
     end
 
@@ -131,15 +133,16 @@ module AlfredAC
               .quicklookurl(villager_url)
               .arg(villager_url)
               .text('copy', villager_url)
+              # .icon(fetch_art(villager))
     end
 
-    def category_json(key, val)
-      # TODO: Autocomplete category without overwriting villager
+    def trait_json(key, val)
       workflow.result
               .title(key)
               .subtitle(val)
-              .icon(icon_name(key))
-      # TODO: Different links for mod
+              .autocomplete("#{query.capitalize} #{key}") # TODO: Extract into method.
+              .icon(icon_path(key))
+              .arg(full_url(query))
     end
 
     def no_match_json
@@ -149,11 +152,27 @@ module AlfredAC
     end
 
     def titleize(str)
-      regex = /[\s -]/
+      pattern = Regexp.new("[\s -]")
 
-      return str.capitalize unless str =~ regex
-      delimiter = str.each_char.select { |c| c =~ regex }
-      str.split(regex).map(&:capitalize).join(delimiter.first)
+      return str.capitalize unless str =~ pattern
+      delimiter = str.each_char.select { |c| c =~ pattern }
+      str.split(pattern).map(&:capitalize).join(delimiter.first)
+    end
+
+    def download_art(villager, cache_path, art_path)
+      # TODO: Fix Code 1: /System/Library/Frameworks/Ruby.framework/Versions/2.0/usr/lib/ruby/2.0.0/net/http.rb:921:in `connect': SSL_connect returned=1 errno=0 state=SSLv2/v3 read server hello A: tlsv1 alert protocol version (OpenSSL::SSL::SSLError)
+
+      file = open(info[villager]['image'])
+      IO.copy_stream(file, "#{cache_path}/#{song.id}.png")
+
+      art_path
+    end
+
+    def fetch_art(villager)
+      home_path = ENV['HOME']
+      cache_path = "#{home_path}/Library/Caches/com.runningwithcrayons.Alfred-3/extensions_tmp"
+      art_path = "#{cache_path}/#{villager}.png"
+      File.exist?(art_path) ? art_path : download_art(villager, cache_path, art_path)
     end
   end
 end
